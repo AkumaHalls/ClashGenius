40dd31783f359862377c8a942dca41899cd3fa73
AkumaHalls <153091442+AkumaHalls@users.noreply.github.com>
2026-03-02 18:01:09 -0300

    Update cwl_planner_cog.py

--- cogs/cwl_planner_cog.py
+++ cogs/cwl_planner_cog.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-1
 import logging
 import discord
 from discord.ext import commands, tasks
@@ -700,3 +700,4 @@ class CwlPlannerCog(commands.Cog, name="CWLPlanner"):
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(CwlPlannerCog(bot))
+
